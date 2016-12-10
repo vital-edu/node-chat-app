769f8d1950978f6d714386684a13e34e7754dcdc
Eduardo Vital <vitaldu@gmail.com>
2016-12-10 09:11:22 -0200

    Add connect and cisconnect handlers to websocket
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path  = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', (soket) => {
+    console.log('User was disconnected');
+  });
+});
+
+
+
+server.listen(port, () => {
   console.log(`The server is up on port ${port}`);
 })
-
-
