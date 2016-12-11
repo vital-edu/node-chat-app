76ff06fa5803237b872e440de011043bfd75a52a
Eduardo Vital <vitaldu@gmail.com>
2016-12-11 15:48:38 -0200

    Emit newMessage when received by server
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/server.js
+++ server/server.js
@@ -15,12 +15,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.on('createMessage', (message) => {
+    console.log('createMessage: ', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  })
+
   socket.on('disconnect', (soket) => {
     console.log('User was disconnected');
   });
 });
-
-
 
 server.listen(port, () => {
   console.log(`The server is up on port ${port}`);
