e28aa4779559d378a3b3146c044cbb90d17f4049
Eduardo Vital <vitaldu@gmail.com>
2016-12-12 17:37:37 -0200

    Send messages only to people in same room
    Signed-off-by: Eduardo Vital <vitaldu@gmail.com>

--- server/server.js
+++ server/server.js
@@ -47,15 +47,21 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log('createMessage: ', message);
+    let user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(message.from, message.text));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
 
-    callback('This is from the server.');
+    callback();
   })
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    let user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', (soket) => {
